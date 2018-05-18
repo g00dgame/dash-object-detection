180c4d9f15e6459d445e89e75392585008b9e85c
Xing Han Lu <xhlperso@gmail.com>
2018-05-18 14:59:50 -0400

    Removed non-relevant files
    Unstable version, to be tested on Heroku.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import ExampleComponent from './components/ExampleComponent.react';
-import my_Player from './components/my_Player.react';
-
-export {
-    ExampleComponent,
-    my_Player
-
-};
